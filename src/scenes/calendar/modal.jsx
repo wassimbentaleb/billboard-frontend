004206397b6ea69841640b900b022d7cab594bdc
Wassim Ben Taleb <wassimwbt@gmail.com>
2024-03-13 01:52:31 +0100

    Modal ui v1.1

--- src/scenes/calendar/modal.jsx
+++ src/scenes/calendar/modal.jsx
@@ -6,13 +6,26 @@ import {
   Box,
   Card,
   Button,
-  Container,
+
 } from "@mui/material";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import { styled } from '@mui/material/styles';
 //import Card from '@mui/material/Card';
 //import CardContent from '@mui/material/CardContent';
 
 // Define a type for the form data
+
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+});
 
 const ImageUpload = (props) => {
   const [imagePreview, setImagePreview] = useState();
@@ -89,8 +102,10 @@ const ImageUpload = (props) => {
             variant="contained"
             color="primary"
             startIcon={<CloudUploadIcon />}
+            component="label"
           >
             Upload image
+            <VisuallyHiddenInput type="file" />
           </Button>
         </Box>
 
